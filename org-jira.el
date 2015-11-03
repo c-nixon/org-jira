2ce8d907fa852470237409db7e8c5ef0b1446031
Chris Nixon <chris.nixon@sigma.me.uk>
2015-11-03 21:40:05 +0000

    Split up update-issues

--- org-jira.el
+++ org-jira.el
@@ -401,10 +401,11 @@ With a prefix argument, allow you to customize the jql.  See
       (delete-region (point-min) (point-max))
 
       (mapc (lambda (issue)
-              (let ((issue-id (org-jira-get-issue-val 'key issue))
-                    (issue-summary (org-jira-get-issue-val 'summary issue)))
+              (let* ((issue-id (cdr (assoc 'key issue)))
+                     (fields (cdr(assoc 'fields issue)))
+                     (issue-summary (cdr (assoc 'summary fields))))
                 (insert (format "- [jira:%s] %s\n" issue-id issue-summary))))
-            issues))
+            (append (cdr (assoc 'issues issues)) nil)))
     (switch-to-buffer issues-headonly-buffer)))
 
 ;;;###autoload
@@ -423,84 +424,100 @@ See`org-jira-get-issue-list'"
 
   (interactive
    (org-jira-get-issue-list))
+  (org-jira--get-issues issues))
+
+
+(defun org-jira--get-issues (issues)
+
   (let (project-buffer)
     (mapc (lambda (issue)
-            (let* ((fields (cdr(assoc 'fields issue)))
-                   (proj-key (cdr (assoc 'key (assoc 'project fields))))
-                   (issue-id (cdr (assoc 'key issue)))
-                   (issue-summary (cdr (assoc 'summary fields)))
-                   (issue-headline issue-summary))
+            (let* ((issue-id (cdr (assoc 'key issue)))
+                   (fields (cdr(assoc 'fields issue)))
+                   (proj-key (cdr (assoc 'key (assoc 'project fields)))))
               (let ((project-file (expand-file-name (concat proj-key ".org") org-jira-working-dir)))
                 (setq project-buffer (or (find-buffer-visiting project-file)
                                          (find-file project-file)))
                 (with-current-buffer project-buffer
-                  (org-jira-mode t)
-                  (widen)
-                  (show-all)
-                  (goto-char (point-min))
-                  (setq p (org-find-entry-with-id issue-id))
-                  (save-restriction
-                    (if (and p (>= p (point-min))
-                             (<= p (point-max)))
-                        (progn
-                          (goto-char p)
-                          (forward-thing 'whitespace)
-                          (kill-line))
-                      (goto-char (point-max))
-                      (unless (looking-at "^")
-                        (insert "\n"))
-                      (insert "* "))
-                    (let ((status (org-jira-get-issue-val 'status fields)))
-                      (insert (concat (cond (org-jira-use-status-as-todo
-                                             (upcase (replace-regexp-in-string " " "-" status)))
-                                            ((member status org-jira-done-states) "DONE")
-                                            ("TODO")) " "
-                                            issue-headline)))
-                    (save-excursion
-                      (unless (search-forward "\n" (point-max) 1)
-                        (insert "\n")))
-                    (org-narrow-to-subtree)
-                    (org-change-tag-in-region
-                     (point-min)
-                     (save-excursion
-                       (forward-line 1)
-                       (point))
-                     (replace-regexp-in-string "-" "_" issue-id)
-                     nil)
-
-                    (mapc (lambda (entry)
-                            (let ((val (org-jira-get-issue-val entry fields)))
-                              (when (and val (not (string= val "")))
-                                (org-entry-put (point) (symbol-name entry) val))))
-                          '(assignee reporter issuetype priority resolution status components created updated))
-                    (org-entry-put (point) "ID" issue-id)
-
-                    (mapc (lambda (heading-entry)
-                            (ensure-on-issue-id
-                                issue-id
-                              (let* ((entry-heading (concat (symbol-name heading-entry) (format ": [[%s][%s]]" (concat jiralib-url "/browse/" issue-id) issue-id))))
-                                (setq p (org-find-exact-headline-in-buffer entry-heading))
-                                (if (and p (>= p (point-min))
-                                         (<= p (point-max)))
-                                    (progn
-                                      (goto-char p)
-                                      (org-narrow-to-subtree)
-                                      (goto-char (point-min))
-                                      (forward-line 1)
-                                      (delete-region (point) (point-max)))
-                                  (if (org-goto-first-child)
-                                      (org-insert-heading)
-                                    (goto-char (point-max))
-                                    (org-insert-subheading t))
-                                  (insert entry-heading "\n"))
-
-                                (insert (replace-regexp-in-string "^" "  " (org-jira-get-issue-val heading-entry fields))))))
-                          '(description))
-                    (org-jira-update-comments-for-current-issue)
-                    (org-jira-update-worklogs-for-current-issue)
-                    )))))
+                  (org-jira-write-issue issue-id fields)
+                  ))))
           (cdr issues))
     (switch-to-buffer project-buffer)))
+
+(defun org-jira-write-issue (issue-id fields)
+  (let* ((issue-summary (cdr (assoc 'summary fields)))
+         (issue-headline issue-summary))
+
+    (org-jira-mode t)
+    (widen)
+    (show-all)
+    (goto-char (point-min))
+    (save-restriction
+      (org-jira-write-issue-header (org-find-entry-with-id issue-id)
+                                   (org-jira-get-issue-val 'status fields))
+
+      ;; Set up issue Properties
+      (mapc (lambda (entry)
+              (let ((val (org-jira-get-issue-val entry fields)))
+                (when (and val (not (string= val "")))
+                  (org-entry-put (point) (symbol-name entry) val))))
+            '(assignee reporter issuetype priority resolution status components created updated))
+      (org-entry-put (point) "ID" issue-id)
+
+      (org-jira-write-issue-headings '(description))
+
+      (org-jira-update-comments-for-current-issue)
+      (org-jira-update-worklogs-for-current-issue))))
+
+(defun org-jira-write-issue-header (issue-point status)
+  (if (and issue-point (>= issue-point (point-min))
+           (<= issue-point (point-max)))
+      (progn
+        (goto-char issue-point)
+        (forward-thing 'whitespace)
+        (kill-line))
+    (goto-char (point-max))
+    (unless (looking-at "^")
+      (insert "\n"))
+    (insert "* "))
+  (insert (concat (cond (org-jira-use-status-as-todo
+                         (upcase (replace-regexp-in-string " " "-" status)))
+                        ((member status org-jira-done-states) "DONE")
+                        ("TODO")) " "
+                        issue-headline))
+  (save-excursion
+    (unless (search-forward "\n" (point-max) 1)
+      (insert "\n")))
+  (org-narrow-to-subtree)
+  (org-change-tag-in-region
+   (point-min)
+   (save-excursion
+     (forward-line 1)
+     (point))
+   (replace-regexp-in-string "-" "_" issue-id)
+   nil))
+
+(defun org-jira-write-issue-headings (headings)
+  (mapc (lambda (heading-entry)
+          (ensure-on-issue-id
+              issue-id
+            (let* ((entry-heading (concat (symbol-name heading-entry) (format ": [[%s][%s]]" (concat jiralib-url "/browse/" issue-id) issue-id))))
+              (setq p (org-find-exact-headline-in-buffer entry-heading))
+              (if (and p (>= p (point-min))
+                       (<= p (point-max)))
+                  (progn
+                    (goto-char p)
+                    (org-narrow-to-subtree)
+                    (goto-char (point-min))
+                    (forward-line 1)
+                    (delete-region (point) (point-max)))
+                (if (org-goto-first-child)
+                    (org-insert-heading)
+                  (goto-char (point-max))
+                  (org-insert-subheading t))
+                (insert entry-heading "\n"))
+
+              (insert (replace-regexp-in-string "^" "  " (org-jira-get-issue-val heading-entry fields))))))
+        headings))
 
 ;;;###autoload
 (defun org-jira-update-comment ()
